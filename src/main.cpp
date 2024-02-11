b5c9aeb46f3ecc497b96ee7a296f35ed10889b0c
k1105 <kanapon1105@yahoo.co.jp>
2024-02-11 23:25:18 +0900

    apply gitignore

--- src/main.cpp
+++
@@ -1,8 +0,0 @@
-#include "ofMain.h"
-#include "ofApp.h"
-
-//========================================================================
-int main( ){
-    ofSetupOpenGL(1024,768,OF_WINDOW); // ウィンドウのサイズを設定
-    ofRunApp(new ofApp());
-}
